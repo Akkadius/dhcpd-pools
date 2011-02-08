7e38c27012ce954d448f1ac8cdfabd2554423f3d
Sami Kerola <kerolasa@iki.fi>
2011-02-08 19:30:57 +0100

    autogen.sh: test bug fix & clean up to messages
    The test had == used as = which is bashisms, and does not work on
    system, such as Ubuntu, where /bin/sh is dash or some other
    non-bash shell.
    
    Signed-off-by: Sami Kerola <kerolasa@iki.fi>

--- autogen.sh
+++ autogen.sh
@@ -13,46 +13,45 @@ test -z "$srcdir" && srcdir=.
 THEDIR=`pwd`
 cd $srcdir
 DIE=0
-HAS_GTKDOC=1
 
 test -f src/dhcpd-pools.c || {
 	echo
-	echo "You must run this script in the top-level util-linux directory"
+	echo "You must run this script in the top-level dhcpd-pools directory"
 	echo
 	DIE=1
 }
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have autoconf installed to generate util-linux build system."
+	echo "You must have autoconf installed to generate dhcpd-pools build system."
 	echo
 	DIE=1
 }
 (autoheader --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have autoheader installed to generate util-linux build system."
+	echo "You must have autoheader installed to generate dhcpd-pools build system."
 	echo "The autoheader command is part of the GNU autoconf package."
 	echo
 	DIE=1
 }
 (libtool --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have libtool-2 installed to generate util-linux build system."
+	echo "You must have libtool-2 installed to generate dhcpd-pools build system."
 	echo
 	DIE=1
 }
 (automake --version) < /dev/null > /dev/null 2>&1 || {
 	echo
-	echo "You must have automake installed to generate util-linux build system."
+	echo "You must have automake installed to generate dhcpd-pools build system."
 	echo 
 	DIE=1
 }
 
 ltver=$(libtoolize --version | awk '/^libtoolize/ { print $4 }')
-test ${ltver##2.} == "$ltver" && {
+test ${ltver##2.} = "$ltver" && {
 	echo "You must have libtool version >= 2.x.x, but you have $ltver."
 	DIE=1
-}
+±}
 
 if test "$DIE" -eq 1; then
 	exit 1
@@ -71,7 +70,6 @@ libtoolize --force $LT_OPTS
 aclocal $AL_OPTS
 autoconf $AC_OPTS
 autoheader $AH_OPTS
-
 automake --add-missing $AM_OPTS
 
 cd $THEDIR
@@ -79,5 +77,3 @@ cd $THEDIR
 echo
 echo "Now type '$srcdir/configure' and 'make' to compile."
 echo
-
-
