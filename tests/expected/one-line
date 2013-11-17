Akkadius/dhcpd-pools

commit 7fc354827a4598adb008bc94f94d40fff111cd61

getdata: fix consecutive range definition regression
The added test demonstrates the issue.  If a did not end with whitespace
it was skipped.

Reported-by: Fredrik Lysén <fredrik.lysen@uadm.uu.se>
Signed-off-by: Sami Kerola <kerolasa@iki.fi>

tests/expected/one-line
@@ -1,0 +1,1 @@
+simple

tests/expected/simple
@@ -1,6 +1,7 @@
 Ranges:
 shared net name     first ip           last ip            max   cur    percent  touch   t+c  t+c perc
-All networks        10.0.0.1         - 10.0.0.10           10    10    100.000      0    10   100.000
+All networks        10.0.0.1         - 10.0.0.5             5     5    100.000      0     5   100.000
+All networks        10.0.0.6         - 10.0.0.10            5     5    100.000      0     5   100.000
 
 Shared networks:
 name                   max   cur     percent  touch    t+c  t+c perc

tests/confs/one-line
@@ -1,0 +1,1 @@
+pool {range 10.0.0.1 10.0.0.5;range 10.0.0.6 10.0.0.10;}

tests/leases/one-line
@@ -1,0 +1,1 @@
+simple

tests/one-line
@@ -1,0 +1,1 @@
+test.sh

src/getdata.c
@@ -263,6 +263,7 @@ if (posix_fadvise(fileno(dhcpd_config), 0, 0, POSIX_FADV_SEQUENTIAL) != 0) {
 				 *
 				 * ...to be interpreted correctly. */
 				c = ' ';
+				break;
 			} else if (argument == ITS_A_RANGE_SECOND_IP && i == 0) {
 				range_p->last_ip = range_p->first_ip;
 				goto newrange;

tests/Makemodule.am
@@ -14,6 +14,7 @@ TESTS = \
 	tests/full-json \
 	tests/leading0 \
 	tests/one-ip \
+	tests/one-line \
 	tests/same-twice \
 	tests/simple \
 	tests/v6

commit ca0b1c326246fc609390b6390ff4cc0e5d8caabc

tests: fix testing error
The commit 7fc354827a4598adb008bc94f94d40fff111cd61 introduced an symlink
that end up to update simple test causing it to break.

Signed-off-by: Sami Kerola <kerolasa@iki.fi>

tests/expected/one-line
@@ -1,1 +1,11 @@
-simple
+Ranges:
+shared net name     first ip           last ip            max   cur    percent  touch   t+c  t+c perc
+All networks        10.0.0.1         - 10.0.0.5             5     5    100.000      0     5   100.000
+All networks        10.0.0.6         - 10.0.0.10            5     5    100.000      0     5   100.000
+
+Shared networks:
+name                   max   cur     percent  touch    t+c  t+c perc
+
+Sum of all ranges:
+name                   max   cur     percent  touch    t+c  t+c perc
+All networks            10    10    100.000       0     10   100.000

tests/expected/simple
@@ -1,7 +1,6 @@
 Ranges:
 shared net name     first ip           last ip            max   cur    percent  touch   t+c  t+c perc
-All networks        10.0.0.1         - 10.0.0.5             5     5    100.000      0     5   100.000
-All networks        10.0.0.6         - 10.0.0.10            5     5    100.000      0     5   100.000
+All networks        10.0.0.1         - 10.0.0.10           10    10    100.000      0    10   100.000
 
 Shared networks:
 name                   max   cur     percent  touch    t+c  t+c perc